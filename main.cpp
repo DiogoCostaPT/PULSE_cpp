06d636b7c210fc879395d843982833e40c2772b4
Diogo Costa <diogo.costa@usask.ca>
2019-12-06 18:42:51 -0600

    Baseline working (seems like Matlab version now)

--- main.cpp
+++ main.cpp
@@ -152,16 +152,12 @@ int findLastStep(const char *path) {
         free(simnum_str_i);
    }
    
-   //free(filename_i);
    free(entry);
-   //free(dir);
-   //std::cout << "Start time (s): " << timestart << " (initial conditions available)" << std::endl;
    return timestart;
 }
 
 int read_simset(globalpar& gp,std::string* sim_purp, int *H_local,int *L_local, int *h_layer,int *l_layer, std::string* qmelt_file,std::ofstream* logPULSEfile)
 {
-    // read_modset(ds,print_step,ks_input,zbinc,ntim_days)
     
     std::string str, modset_flname, msg;
     modset_flname = "simset.pulse";
@@ -214,7 +210,6 @@ void read_qmelt(globalpar& gp,globalvar& gv,std::string* qmelt_file,std::ofstrea
             vmelt = filedataQ(a,1);  // value of melt
             (*gv.qmelt).at(a,0) = tmelts;  
             (*gv.qmelt).at(a,1) = vmelt;
-            //gv.qmelt += vmelt /(1000.*3600.*24.) * (tmelts - tmelts_bef); 
         }
        (gp.Tperd) = tmelts;
        msg = "Successful loading the file: " + (*qmelt_file);
@@ -255,7 +250,6 @@ void wettingfront_cell_location(globalpar& gp,globalvar& gv,double *v, double *d
     gp.wetfront_z = std::fmax(gp.wetfront_z - (*v) * (*deltt),0.f);
     int tmp = std::round(nh_l-gp.wetfront_z/gv.snowh);
     *wetfront_cell_new = std::min(tmp,nh_l); // finding the cell when the wetting front is located
-    //wetfront_cell = (*wetfront_cell_new);
     
 }
 
@@ -268,7 +262,7 @@ void Crank_Nicholson(globalvar& gv,int *upperboundary_cell_new, int *wetfront_ce
 
     // to solve A.x1=B.x0
     int nli = gv.nl;
-    int nhi = *wetfront_cell_new-*upperboundary_cell_new;                   // the boundaries are knowns, so don't need to be included in matrix A
+    int nhi = *wetfront_cell_new-*upperboundary_cell_new+1;                   // the boundaries are knowns, so don't need to be included in matrix A
     int nt = nli*nhi;
     double k1 = (*v)*(*deltt)/(4*gv.snowh);       // constants for Crank-Nicholson scheme
     double k2 = (*D)*(*deltt)/(2*pow(gv.snowh,2));     // constants for Crank-Nicholson scheme
@@ -280,7 +274,6 @@ void Crank_Nicholson(globalvar& gv,int *upperboundary_cell_new, int *wetfront_ce
     double a1=-(k1+k2);
     double a2=(1+2*k2+2*k3);
     double a3=k1-k2;
-    //A=zeros(nt,nt);
 
     // 1) all domain (diagonals: -9,-1,0,1,9)
     arma::mat A = a3*(arma::diagmat(arma::ones(1,nt-nli),nli))
@@ -290,7 +283,7 @@ void Crank_Nicholson(globalvar& gv,int *upperboundary_cell_new, int *wetfront_ce
                     -k3*arma::diagmat(arma::ones(1,nt-1),-1);
 
 
-    for(il=0;il<=(nt/nli)-2 ;il++){ // inner cells - left and right marigns
+    for(il=0;il<(nt/nli)-1 ;il++){ // inner cells - left and right marigns
         A(il*nli+1,il*nli)=0;
         A(il*nli+1,il*nli+1)=A(il*nli+1,il*nli+1)-k3;
         A(il*nli+nli,il*nli+nli+1)=0;
@@ -298,32 +291,29 @@ void Crank_Nicholson(globalvar& gv,int *upperboundary_cell_new, int *wetfront_ce
     }
 
     // 2) first row (y=1)
-    A(arma::span(0,nli-1),arma::span(0,nli-1)) += a1*arma::diagmat(arma::ones(1,nli)); //diagonal
+    A(arma::span(0,nli-1),arma::span(0,nli-1)) = a1*arma::diagmat(arma::ones(1,nli)); //diagonal
     A(0,0)=a1+a2-k3; // left corner
     A(nli-1,nli-1)=a2+a1-k3; // left corner
-    A(nli-1,nli)=0; // left corner
 
     
     // 3) last row (y=ny)
     A(arma::span(nt-nli,nt-1),arma::span(nt-nli,nt-1)) = (a2+a3)*arma::diagmat(arma::ones(1,nli)); // diagonal !!!! CHECK IF IT SHOULDN'T BE "+=" (LINE ABOVE) INSTEAD OF "="
     A(nt-1,nt-1)=a2+a3-k3; // right corner
     A(nt-nli,nt-nli)=a3+a2-k3; // left corner
-    A(nt-nli,nt-nli-1)=0; // left corner
 
 
     // Creating B
     double a4=(1-2*k2-2*k3);
-    //B=zeros(nt,nt);
 
     // 1) all domain (diagonals: -9,-1,0,1,9)
-    arma::mat B=-a3*arma::diagmat(arma::ones(1,nt-nli),nli)-
-            a1*arma::diagmat(arma::ones(1,nt-nli),-(nli))+
-            a4*arma::diagmat(arma::ones(1,nt))+
-            k3*arma::diagmat(arma::ones(1,nt-1),1)+
-            k3*arma::diagmat(arma::ones(1,nt-1),-1);
+    arma::mat B=-a3*arma::diagmat(arma::ones(1,nt-nli),nli)
+            -a1*arma::diagmat(arma::ones(1,nt-nli),-(nli))
+            +a4*arma::diagmat(arma::ones(1,nt))
+            +k3*arma::diagmat(arma::ones(1,nt-1),1)
+            +k3*arma::diagmat(arma::ones(1,nt-1),-1);
 
 
-    for(il=0;il<=(nt/nli)-2 ;il++){         // inner cells - left and right marigns
+    for(il=0;il<(nt/nli)-1 ;il++){         // inner cells - left and right marigns
         B(il*nli+1,il*nli)=0;
         B(il*nli+1,il*nli+1)=A(il*nli+1,il*nli+1)-k3;
         B(il*nli+nli,il*nli+nli+1)=0;
@@ -331,44 +321,20 @@ void Crank_Nicholson(globalvar& gv,int *upperboundary_cell_new, int *wetfront_ce
     }
 
     // 2) first row (y=1)
-    B(arma::span(0,nli-1),arma::span(0,nli-1)) += (-a1)*arma::diagmat(arma::ones(1,nli)); //diagonal
+    B(arma::span(0,nli-1),arma::span(0,nli-1)) = (-a1)*arma::diagmat(arma::ones(1,nli)); //diagonal
     B(0,0)=-a1+a4+k3; // left corner
     B(nli-1,nli-1)=a4-a1+k3; // left corner
-    B(nli-1,nli)=0; // left corner
 
     // 3) last row (y=ny)
     B(arma::span(nt-nli,nt-1),arma::span(nt-nli,nt-1)) =(a4-a3)*arma::diagmat(arma::ones(1,nli)); // diagonal !!!! CHECK IF IT SHOULDN'T BE "+=" (LINE ABOVE) INSTEAD OF "="
     B(nt-1,nt-1)=a4-a3+k3; // right corner
     B(nt-nli,nt-nli)=-a3+a4+k3; // left corner
-    B(nt-nli,nt-nli-1)=0; // left corner
-
-    //c_m_new = c_m_prev;
-    
-    arma::mat c1 = arma::reshape((*gv.c_m)(arma::span(0,nli-1),arma::span(*upperboundary_cell_new,*upperboundary_cell_new+nhi-1)),1,nt);  //c1=reshape(c_m_prev(2:end-1,2:end-1),1,[]);
+   
+    arma::mat c1 = arma::reshape((*gv.c_m)(arma::span(0,nli-1),arma::span(*upperboundary_cell_new,*wetfront_cell_new)),1,nt);  //c1=reshape(c_m_prev(2:end-1,2:end-1),1,[]);
     arma::mat b=B*trans(c1);    // calculation of [b]
     arma::mat c2=arma::solve(A,b);     // calculation of c
-    (*gv.c_m)(arma::span(0,nli-1),arma::span(*upperboundary_cell_new,*upperboundary_cell_new+nhi-1)) = arma::reshape(c2,nli,nhi);
+    (*gv.c_m)(arma::span(0,nli-1),arma::span(*upperboundary_cell_new,*wetfront_cell_new)) = arma::reshape(c2,nli,nhi);
 
-
-    for(il=0;il<=nli ;il++){ // to avoid back flow due to roundoff errors
-        for(ih=0;ih<=nhi ;ih++){ 
-             if ((*gv.c_m).at(il,ih)<0){
-                 (*gv.c_m).at(il,ih)=0;
-            }
-        }
-    }
-    
-        //for(il=0;il<=gv.nl ;il++){
-        //        for(ih=0;ih<=gv.nh ;ih++){
-        //            if ((*gv.c_i).at(il,ih) > 10000){
-                        std::cout << "c_i_2 =  " + std::to_string((*gv.c_i).at(il,ih)) << std::endl;
-        //            }
-        //             if ((*gv.c_m).at(il,ih) > 10000){
-                        std::cout << "c_m_2 =  " + std::to_string((*gv.c_i).at(il,ih)) << std::endl;
-        //            }
-        //        }
-        //}
-    
 }
 
 
@@ -472,13 +438,6 @@ void PULSEmodel(globalpar& gp,globalvar& gv,std::ofstream* logPULSEfile)
         // limiting the flux to the wetting front (uses intersticial velocity to determine the wetting front)
         wettingfront_cell_location(gp,gv,&v,&deltt,&wetfront_cell_new,&wetfront_cell_prev);
         
-        // Getting the active layers
-        //-// arma::mat c_m_prev_i = (*gv.c_m).rows(1,wetfront_cell_prev); // moved inside the solver now in cpp
-        //-// arma::mat c_i_prev_i = (*gv.c_i).rows(1,wetfront_cell_prev); // moved inside the solver now in cpp
-        //-// arma::mat c_s_prev_i = (*gv.c_s).rows(1,wetfront_cell_prev); // moved inside the solver now in cpp
-        //c_m_new_i=c_m(:,1:wetfront_cell,t);
-        //c_i_new_i=c_i(:,1:wetfront_cell,t);
-        //c_s_new_i=c_s(:,1:wetfront_cell,t);
 
         // Melting velocity: last cell
         upperboundary_cell_prev = upperboundary_cell; // wetting fron cell in the previous time step
@@ -494,56 +453,19 @@ void PULSEmodel(globalpar& gp,globalvar& gv,std::ofstream* logPULSEfile)
             print_screen_log(logPULSEfile,&msg);
 
             abort();
-        //-// }else{ // moved inside the solver now in cpp
-        //-//     if (wetfront_cell_new == wetfront_cell_prev + 1){ // moved inside the solver now in cpp
-             //-// c_m_prev_i =  arma::join_vert(c_m_prev_i,arma::zeros(nl_l,1));  // moved inside the solver now in cpp
-             //c_i_prev_i = [c_i_prev_i(:,wetfront_cell_prev),zeros(nl,1)]; 
-             //c_s_prev_i = [c_s_prev_i(:,wetfront_cell_prev),ones(nl,1)*c0_s];
-             //-// arma::mat c_m_new_i = c_m_prev_i; // moved inside the solver now in cpp
-
-             //arma::mat c_i_new_i = [c_i_prev_i(:,1:wetfront_cell_prev),zeros(nl,1)]; 
-             //arma::mat c_s_new_i = [c_s_prev_i(:,1:wetfront_cell_prev),c_s(:,wetfront_cell_prev+1)];
-
-             //-// arma::mat c_i_new_i = arma::join_vert(c_i_prev_i,arma::zeros(nl_l,1));  // moved inside the solver now in cpp
-             //-// arma::mat c_s_new_i = arma::join_vert(c_s_prev_i,(*gv.c_s).row(wetfront_cell_prev+1));  // moved inside the solver now in cpp
-           
-        //-//     }else{ // moved inside the solver now in cpp
-        //-//      arma::mat c_s_new_i = c_s_prev_i;
-        //-//      arma::mat c_i_new_i = c_i_prev_i;
-        //-//      arma::mat c_m_new_i = c_m_prev_i;
-        //-//  } 
+       
         }
 
         if (gv.porosity_m < 1-gp.num_stblty_thrshld_prsity && gv.porosity_i > gp.num_stblty_thrshld_prsity && gv.porosity_s > gp.num_stblty_thrshld_prsity){
             if (wetfront_cell_new > 5){
-                //int nh_Crank = wetfront_cell_new - 1;        
-                //_m_prev_ii = c_m_prev_i(:,1:nh_Crank);
-                //c_m_prev_ii = c_m_prev_i(:,1:wetfront_cell_new);
 
-                // c_m_comp = Crank_Nicholson(t,nl,wetfront_cell_new,c_m_prev_ii,snowl,snowh,deltt,v(t),D(t)); % solve advection and dispersion in the mobile zone
                 Crank_Nicholson(gv,&upperboundary_cell_new, &wetfront_cell_new,&deltt,&v,&D); // solve advection and dispersion in the mobile zone
 
-                // Computing the wet front - it needs to be calculated seperatly from
                 // Crank Nicolson to limit the fluxes across boundaries
 
                (*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new) -= v * deltt * ((*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new) - (*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new-1))/gv.snowh ; // compute onh advection to the wetting front
 
-               //c_m_new_i(:,wetfront_cell_new) = c_m_new_i_wetfront;
-
-               // fluxback = sum(c_m(:,wetfront_cell+1:end,t)');
-               //c_m(:,wetfront_cell-1,t) = c_m(:,wetfront_cell-1,t) + fluxback';
-
-              //-//}else{
-                  // do nothing, onh add the sources
-              //-//    c_m_new_i =  c_m_prev_i;
             }
-
-            // c_i_new_i = c_i_prev_i;
-            // c_s_new_i = c_s_prev_i;
-
-            // Effect of freeze-thaw
-            // exchange = (c_s_new_i -  c_m_new_i) * rho_s/rho_m * q; %dporosity_s_dt;
-            // exchange = c_s_new_i * rho_s/rho_m ; %dporosity_s_dt;
 
             (*gv.exchange_si) = (*gv.c_s) * gp.rho_s/gp.rho_m * q * deltt / wetfront_cell_new;
 
@@ -564,31 +486,21 @@ void PULSEmodel(globalpar& gp,globalvar& gv,std::ofstream* logPULSEfile)
                 }
             };
           
-                 
-            // c_m(:,wetfront_cell,t) = c_m(:,wetfront_cell,t) + u * c_s(:,wetfront_cell,t) / rho_m;
-            // c_i_new_i =  c_i_new_i + exchange * deltt / wetfront_cell(t); % / porosity_m(t); 
             (*gv.c_i) =  ( (*gv.c_i) * gv.porosity_i_prev + (*gv.exchange_si) * gv.porosity_s_prev ) / gv.porosity_i; // / porosity_m(t);
             (*gv.c_s) = ( (*gv.c_s) * gv.porosity_s_prev - (*gv.exchange_si) * gv.porosity_s_prev ) / gv.porosity_s;
-            //c_s_new_i = c_s_new_i  - exchange ; 
-
             
             // Exchange with immobile phase (just exchange)
             (*gv.exchange_im)  = deltt * (gp.alphaIE/gv.porosity_m_prev * ((*gv.c_i) - (*gv.c_m))) ; 
-            
-            
-            
-            // exchange = alpha .* (c_i_new_i - c_m_new_i) ; 
+                 
             // limit the flux to the available material
             
             // limit the flux to the available material
-            for(il=0;il<=gv.nl ;il++){
-                for(ih=0;ih<=gv.nh ;ih++){
+            for(il=0;il<gv.nl ;il++){
+                for(ih=0;ih<gv.nh ;ih++){
                      if ((*gv.exchange_im).at(il,ih) > 0){
                         (*gv.exchange_im).at(il,ih) = std::min((*gv.exchange_im).at(il,ih),(*gv.c_i).at(il,ih));
                     }else if((*gv.exchange_im).at(il,ih) < 0){
                         (*gv.exchange_im).at(il,ih) = -(std::min(std::abs((*gv.exchange_im).at(il,ih)),std::abs((*gv.c_m).at(il,ih))));
-                        //msg = "PROBLEM: negative i->m exchange";
-                        //print_screen_log(logPULSEfile,&msg);
                     }
                      if(ih<upperboundary_cell_new || ih>wetfront_cell_new){
                         (*gv.exchange_im).at(il,ih) = 0.0f;
@@ -627,39 +539,6 @@ void PULSEmodel(globalpar& gp,globalvar& gv,std::ofstream* logPULSEfile)
             }
                         
         }
-    
-    
-        //- removed this code; check if it is important) // c_m_new_i(1,:) = c_m_new_i(3,:);
-        //- removed this code; check if it is important) // c_m_new_i(2,:) = c_m_new_i(3,:);   
-        //- removed this code; check if it is important) // c_m_new_i(end,:) = c_m_new_i(end-2,:);   
-        //- removed this code; check if it is important) // c_m_new_i(end-1,:) = c_m_new_i(end-2,:);  
-
-        //- removed this code; check if it is important) // c_i_new_i(1,:) = c_i_new_i(3,:);
-        //- removed this code; check if it is important) // c_i_new_i(2,:) = c_i_new_i(3,:);   
-        //- removed this code; check if it is important) // c_i_new_i(end,:) = c_i_new_i(end-2,:);   
-        //- removed this code; check if it is important) // c_i_new_i(end-1,:) = c_i_new_i(end-2,:);  
-
-        //- removed this code; check if it is important) // c_s_new_i(1,:) = c_s_new_i(3,:);
-        //- removed this code; check if it is important) // c_s_new_i(2,:) = c_s_new_i(3,:);  
-        //- removed this code; check if it is important) // c_s_new_i(end,:) = c_s_new_i(end-2,:);   
-        //- removed this code; check if it is important) // c_s_new_i(end-1,:) = c_s_new_i(end-2,:);  
-
-
-        // Save
-        //c_m(:,1:wetfront_cell_new) = c_m_new_i;
-        //c_i(:,1:wetfront_cell_new) = c_i_new_i;
-        //c_s(:,1:wetfront_cell_new) = c_s_new_i;
-
-        // Compute flux
-        //c_export= [c_export, c_m(round(nl-2/2),end-1)];
-        //cflux = [cflux, c_export(t) * q];
-
-
-
-        //- removed this code; check if it is important) //if (t == 2){
-        //- removed this code; check if it is important) //    fileID = []; // to save the output file ID for subsequent access to save the following time steps
-        //- removed this code; check if it is important) //    tcum_prev = 0;
-        //- removed this code; check if it is important) //}
 
         // Print results                
           if (tcum>=print_next){
