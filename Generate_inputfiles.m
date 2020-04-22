fe91174e372a25b66c81448251f6084f02aae982
DiogoCostaPT <1208buckas>
2020-04-21 18:36:49 -0600

    MATLAB: adding prec chemistry to input files generator

--- Generate_inputfiles.m
+++ Generate_inputfiles.m
@@ -1,150 +1,164 @@
 
 
 folder_loc = '/media/dcosta/data/megasync/ec_main/models/pulse/code/code_matlab_original/Svalbard_Snownet/';
-
-
-%% Generate 0.txt file 
-    
+meteo_file = 'Meteo_2014-2015.xlsx';
 chemistry_file = 'BRG_data.xlsx';
 species = 'NO3';
+    
+gen_0txtfile_flag = 0;    
+gen_prec_file_flag = 1;
 
-dataraw_chem = xlsread([folder_loc,chemistry_file],species);
+%% Generate 0.txt file 
+if gen_0txtfile_flag == 1
 
-time_obs_chem = dataraw_chem(:,1) + 693960;
+    dataraw_chem = xlsread([folder_loc,chemistry_file],species);
 
-depths_obs = [10,20,30,40,50,60,70,80,90,100]; % cm		
+    time_obs_chem = dataraw_chem(:,1) + 693960;
 
-NO3_conc_ppb = dataraw_chem(1,2:end); % ppb
-%NO3_conc_ppb = [281.766753953895,112.96520293409,162.480671212122,102.504849889069,116.591016244802,...
-%    92.9862893794549,141.758823073791,100.783465395752,92.4981427755126,135.784890366924]; % ppb
+    depths_obs = [10,20,30,40,50,60,70,80,90,100]; % cm		
 
-NO3_conc_mgl = NO3_conc_ppb / 1000;
+    NO3_conc_ppb = dataraw_chem(1,2:end); % ppb
+    %NO3_conc_ppb = [281.766753953895,112.96520293409,162.480671212122,102.504849889069,116.591016244802,...
+    %    92.9862893794549,141.758823073791,100.783465395752,92.4981427755126,135.784890366924]; % ppb
 
-snow_L = 10;
-snow_h = 1; % 5 mm
-snow_l = 1; % 1 mm
+    NO3_conc_mgl = NO3_conc_ppb / 1000;
 
-% calc
+    snow_L = 10;
+    snow_h = 1; % 5 mm
+    snow_l = 1; % 1 mm
 
-snow_H = depths_obs(end);
+    % calc
 
-cell_h_num = snow_H/snow_h;
-cell_l_num = snow_L/snow_l;
+    snow_H = depths_obs(end);
 
-depths_obs_flip = fliplr(depths_obs);
+    cell_h_num = snow_H/snow_h;
+    cell_l_num = snow_L/snow_l;
 
-file_0txt = [];
+    depths_obs_flip = fliplr(depths_obs);
 
-cm_0 = 0;
-poros_m = 0.008;
-poros_s = 0.991;
+    file_0txt = [];
 
-for hci = 0:cell_h_num-1
-    for lci = 0:cell_l_num-1
+    cm_0 = 0;
+    poros_m = 0.008;
+    poros_s = 0.991;
 
-    hi = hci * snow_h;
+    for hci = 0:cell_h_num-1
+        for lci = 0:cell_l_num-1
 
-    iloc_max = find(depths_obs_flip>=hi);
+        hi = hci * snow_h;
 
-    cs_0 = NO3_conc_mgl(iloc_max(end));
+        iloc_max = find(depths_obs_flip>=hi);
 
-    file_0txt = [file_0txt;[hci,lci,cm_0,0,cs_0,poros_m,poros_s,0,0]];
+        cs_0 = NO3_conc_mgl(iloc_max(end));
+
+        file_0txt = [file_0txt;[hci,lci,cm_0,0,cs_0,poros_m,poros_s,0,0]];
+
+        end
 
     end
 
 end
-
-
 %% Generate Qmelt data
-   
-meteo_file = 'Meteo_2014-2015.xlsx';
-dataraw_meteo = importdata([folder_loc,meteo_file]);
+if gen_prec_file_flag == 1
+    
+    % meteo data
+    dataraw_meteo = importdata([folder_loc,meteo_file]);
+    time_meteo = dataraw_meteo.data(:,1) + 695422 ;
+    
+    % chem data
+    dataraw_chem = xlsread([folder_loc,chemistry_file],species);
+    depths_obs = [10,20,30,40,50,60,70,80,90,100];
+    time_obs_chem = dataraw_chem(:,1) + 693960;
+    timestart_chem = time_obs_chem(1);
+    timeend_chem = time_obs_chem(end);
+    
+    % sub-set meteo data to chem data time period
+    i_tstart = find(time_meteo==timestart_chem);
+    i_tend = find(time_meteo==timeend_chem);
+    dataraw_meteo_relsubset = dataraw_meteo.data(i_tstart:i_tend,:);
 
-time_meteo = dataraw_meteo.data(:,1) + 695422 ;
+    % prepare data
+    TIME = dataraw_meteo_relsubset(:,1) + 695422;
+    TEMP = dataraw_meteo_relsubset(:,3);
+    RH = dataraw_meteo_relsubset(:,4);
+    PRESS = dataraw_meteo_relsubset(:,5);
+    WS = dataraw_meteo_relsubset(:,6);
+    WD = dataraw_meteo_relsubset(:,7);
+    RAD = dataraw_meteo_relsubset(:,8);
+    PREC = dataraw_meteo_relsubset(:,9);
+    PREC_type = dataraw_meteo_relsubset(:,10);
 
-timestart_chem = time_obs_chem(1);
-timeend_chem = time_obs_chem(end);
+    TEMP_ts = timeseries(TEMP,TIME);
+    RH_ts = timeseries(RH,TIME);
+    PRESS_ts = timeseries(PRESS,TIME);
+    WS_ts = timeseries(WS,TIME);
+    WD_ts = timeseries(WD,TIME);
+    RAD_ts = timeseries(RAD,TIME);
+    PREC_ts = timeseries(PREC,TIME);
+    PREC_type_ts = timeseries(PREC_type,TIME);
 
-i_tstart = find(time_meteo==timestart_chem);
-i_tend = find(time_meteo==timeend_chem);
+    % plot all data
+    figure
+    subplot(3,3,1)
+    plot(TEMP_ts)
+    title('TEMP_ts')
+    grid on
+    subplot(3,3,2)
+    plot(RH_ts)
+    title('RH_ts')
+    grid on
+    subplot(3,3,3)
+    plot(PRESS_ts)
+    title('PRESS_ts')
+    grid on
+    subplot(3,3,4)
+    plot(WS_ts)
+    title('WS_ts')
+    grid on
+    subplot(3,3,5)
+    plot(WD_ts)
+    title('WD_ts')
+    grid on
+    subplot(3,3,6)
+    plot(RAD_ts)
+    title('RAD_ts')
+    grid on
+    subplot(3,3,7)
+    plot(PREC_ts)
+    title('PREC_ts')
+    grid on
+    subplot(3,3,8)
+    plot(PREC_type_ts)
+    title('PREC_type_ts')
+    grid on
 
+    % PREC_type_ts: 2 is liquid, 3 is snow, 30 and 32 a kind of slush snow
+    RAIN_ts = PREC_ts;
+    RAIN_ts.Data(PREC_type_ts.Data~=2) = 0;
 
-dataraw_meteo_relsubset = dataraw_meteo.data(i_tstart:i_tend,:);
-
-TIME = dataraw_meteo_relsubset(:,1) + 695422;
-TEMP = dataraw_meteo_relsubset(:,3);
-RH = dataraw_meteo_relsubset(:,4);
-PRESS = dataraw_meteo_relsubset(:,5);
-WS = dataraw_meteo_relsubset(:,6);
-WD = dataraw_meteo_relsubset(:,7);
-RAD = dataraw_meteo_relsubset(:,8);
-PREC = dataraw_meteo_relsubset(:,9);
-PREC_type = dataraw_meteo_relsubset(:,10);
-
-TEMP_ts = timeseries(TEMP,TIME);
-RH_ts = timeseries(RH,TIME);
-PRESS_ts = timeseries(PRESS,TIME);
-WS_ts = timeseries(WS,TIME);
-WD_ts = timeseries(WD,TIME);
-RAD_ts = timeseries(RAD,TIME);
-PREC_ts = timeseries(PREC,TIME);
-PREC_type_ts = timeseries(PREC_type,TIME);
-
-figure
-subplot(3,3,1)
-plot(TEMP_ts)
-title('TEMP_ts')
-grid on
-subplot(3,3,2)
-plot(RH_ts)
-title('RH_ts')
-grid on
-subplot(3,3,3)
-plot(PRESS_ts)
-title('PRESS_ts')
-grid on
-subplot(3,3,4)
-plot(WS_ts)
-title('WS_ts')
-grid on
-subplot(3,3,5)
-plot(WD_ts)
-title('WD_ts')
-grid on
-subplot(3,3,6)
-plot(RAD_ts)
-title('RAD_ts')
-grid on
-subplot(3,3,7)
-plot(PREC_ts)
-title('PREC_ts')
-grid on
-subplot(3,3,8)
-plot(PREC_type_ts)
-title('PREC_type_ts')
-grid on
-
-% PREC_type_ts: 2 is liquid, 3 is snow, 30 and 32 a kind of slush snow
-RAIN_ts = PREC_ts;
-RAIN_ts.Data(PREC_type_ts.Data~=2) = 0;
-
-SNOWfall_ts = PREC_ts;
-SNOWfall_ts.Data(PREC_type_ts.Data==2) = 0; % daily
-
-
-figure
-plot(RAIN_ts)
-hold on
-plot(SNOWfall_ts)
-legend('RAIN_tc','SNOWfall_tc')
-grid on
-ylabel('mm')
+    SNOWfall_ts = PREC_ts;
+    SNOWfall_ts.Data(PREC_type_ts.Data==2) = 0; % daily
+    
+    % plot precipitation
+    figure
+    plot(RAIN_ts)
+    hold on
+    plot(SNOWfall_ts)
+    legend('RAIN_tc','SNOWfall_tc')
+    grid on
+    ylabel('mm')
 
 
-% for model
+    % data to copy past to model's meteo file (time and prec vol)
+    time_pulse = SNOWfall_ts.Time(1:end);
+    snowaccum_2pulse = SNOWfall_ts.Data/(3600*24); % minus because it is accumulation
 
-time_pulse = [0; cumsum(etime(datevec(SNOWfall_ts.Time(2:end)),datevec(SNOWfall_ts.Time(1:end-1))))];
-time_pulse = SNOWfall_ts.Time(1:end);
-snowaccum_2pulse = SNOWfall_ts.Data/(3600*24); % minus because it is accumulation
+    % now chem data
+    data_toplayer = dataraw_chem(:,2)/1000; % ppb -> ppm
+    snowprec_chem = interp1(time_obs_chem,data_toplayer,time_pulse);
+    
+    % join
+    time_pulse_sec = [0; cumsum(etime(datevec(SNOWfall_ts.Time(2:end)),datevec(SNOWfall_ts.Time(1:end-1))))];
+    Snowfall_file = [time_pulse_sec,snowaccum_2pulse,snowprec_chem];
 
-Snowfall_file = [time_pulse,snowaccum_2pulse,snowaccum_2pulse*0+0.1];
+end
