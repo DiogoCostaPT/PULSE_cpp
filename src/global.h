2ef9fa901bdb31d323b945f6640cfd5fee5717c5
DiogoCostaPT <1208buckas>
2020-04-30 16:41:11 -0600

    Fix to previous commit (removal or dfrac_i)

--- src/global.h
+++ src/global.h
@@ -65,9 +65,9 @@ public:
             snowL = 0.0f, // snowpack horizontal lenght
             snowl = 0.0f, // grid h lenght
             snowh = 0.0f, // grid l lenght
-            vfrac_m=0.008,
+            vfrac_m= 0.0f,
             //vfrac_i= 0.0f,
-            vfrac_s= 1 - vfrac_m - vfrac_i,
+            vfrac_s= 1 - vfrac_m,
             vfrac_m_prev=vfrac_m,
             //vfrac_i_prev=vfrac_i,
             vfrac_s_prev=vfrac_s,
