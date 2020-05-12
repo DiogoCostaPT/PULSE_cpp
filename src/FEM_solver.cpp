3fd7f0de257375d93d94ea51a4a891b5b658b851
DiogoCostaPT <1208buckas>
2020-05-12 16:31:42 -0600

    bug fix in FEM_solver

--- src/FEM_solver.cpp
+++ src/FEM_solver.cpp
@@ -13,7 +13,8 @@ int nhi = gv.wetfront_cell;//-(gv.upperboundary_cell);                   // the 
 int nt = nli*nhi;
 int ih,il;
 
-arma::mat c_new = (*gv.c_m)(arma::span(0,nli-1),arma::span(0,(gv.wetfront_cell)-1));
+arma::mat c_t1 = (*gv.c_m)(arma::span(0,nli-1),arma::span(0,(gv.wetfront_cell)-1));
+arma::mat c_t2 = c_t1;
 
 arma::mat dcdy(nli,gv.wetfront_cell,arma::fill::zeros); 
 arma::mat dc2dy2(nli,gv.wetfront_cell,arma::fill::zeros); 
@@ -29,21 +30,21 @@ for (ih=1;ih<nhi-1;ih++){
             //c1(il,ih-1) -= dtcm;
             //c1(il,ih) += dtcm;
 
-            dcdy(il,ih)=(c_new(il,ih)-c_new(il,ih-1))/gv.snowh;
-            dc2dy2(il,ih)=((c_new(il,ih+1)-c_new(il,ih))/gv.snowh-(c_new(il,ih)-c_new(il,ih-1))/gv.snowh)/(gv.snowh);         
-            dc2dx2(il,ih)=((c_new(il+1,ih)-c_new(il,ih))/gv.snowl-(c_new(il,ih)-c_new(il-1,ih))/gv.snowl)/(gv.snowl);
+            dcdy(il,ih)=(c_t1(il,ih)-c_t1(il,ih-1))/gv.snowh;
+            dc2dy2(il,ih)=((c_t1(il,ih+1)-c_t1(il,ih))/gv.snowh-(c_t1(il,ih)-c_t1(il,ih-1))/gv.snowh)/(gv.snowh);         
+            dc2dx2(il,ih)=((c_t1(il+1,ih)-c_t1(il,ih))/gv.snowl-(c_t1(il,ih)-c_t1(il-1,ih))/gv.snowl)/(gv.snowl);
             dcdt(il,ih)=-(*gv.velc_2d)(il,ih)*dcdy(il,ih)+(*gv.disp_2d)(il,ih)*(dc2dx2(il,ih)+dc2dy2(il,ih));
 
-            c_new(il,ih)=c_new(il,ih)+dcdt(il,ih)*(*deltt);
+            c_t2(il,ih)=c_t2(il,ih)+dcdt(il,ih)*(*deltt);
 
         }
     }
 }
 
 // Von Neumann condition: dc/dx=0 in the river margins
-c_new(0,arma::span(0,nhi-1))=c_new(1,arma::span(0,nhi-1));  
-c_new(nli-1,arma::span(0,nhi-1))=c_new(nli-2,arma::span(0,nhi-1));   
+c_t2(0,arma::span(0,nhi-1))=c_t2(1,arma::span(0,nhi-1));  
+c_t2(nli-1,arma::span(0,nhi-1))=c_t2(nli-2,arma::span(0,nhi-1));   
     
-(*gv.c_m)(arma::span(0,nli-1),arma::span(0,gv.wetfront_cell-1)) =  c_new;
+(*gv.c_m)(arma::span(0,nli-1),arma::span(0,gv.wetfront_cell-1)) =  c_t2;
 
 };
