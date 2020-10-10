796e401ee51a102e107b01d27766b6b17c1fffdc
DiogoCostaPT <1208buckas>
2020-10-09 19:03:52 -0600

    separating the calculations for external and internal snowmodel -> critical to guarantee cleanliness of the code

--- src/pulse.cpp
+++ src/pulse.cpp
@@ -8,184 +8,194 @@
 void pulsemodel(globalpar& gp,globalvar& gv,std::ofstream* logPULSEfile,
         std::string* results_flname)
 {
-    // initiation
-    double tcum = 0.f,
-            //q = 0.f, // melt volume/int
-            t = 1.f, 
-            deltt = 1.0f, // time step calculated from the CFL condition
-            velc = 0.0f, // interstitial flow velocity [m s-1]
-            D = 0.0f; // dispersion coefficient [m2/s]
-    std::string msg;  
-    std::chrono::duration<double> elapsed_seconds;
-    auto start = std::chrono::system_clock::now();
-    auto end = std::chrono::system_clock::now();
-    bool outwritestatus;
-    arma::mat exchange_i;
+// initiation
+double tcum = 0.f,
+        //q = 0.f, // melt volume/int
+        t = 1.f, 
+        deltt = 1.0f, // time step calculated from the CFL condition
+        velc = 0.0f, // interstitial flow velocity [m s-1]
+        D = 0.0f; // dispersion coefficient [m2/s]
+std::string msg;  
+std::chrono::duration<double> elapsed_seconds;
+auto start = std::chrono::system_clock::now();
+auto end = std::chrono::system_clock::now();
+bool outwritestatus;
+arma::mat exchange_i;
+
+unsigned int il,ih,print_next;
     
-    unsigned int il,ih,print_next;
-      
-    tcum = gv.timstart;
-    print_next = tcum + gp.print_step;
-        
-    while (tcum < gp.Tsim)
+tcum = gv.timstart;
+print_next = tcum + gp.print_step;
+
+
+while (tcum < gp.Tsim)
     {
+    
+    t += 1;
 
-        t += 1;
+    // Snow model: internal or external (SNOWPACK or WARD-snow model)
+    if (gp.snowmodel == 0){ // internal 
+            
+                    
+            findInterpMeteo(gv,&tcum);
+            findInterpQmelt(gv,&tcum);
+
+            gv.vfrac_m_prev = gv.vfrac_m;
+            gv.vfrac_s_prev = gv.vfrac_s;
+            //gv.wetfront_cell_prev = gv.wetfront_cell;
+
+            if (gv.qmelt_i==0.0f && gv.rainfall_i == 0.0f){ // accumulation only 
+                deltt = std::fmin(print_next-tcum,
+                    gv.v_swe_freshsnow_max/(std::abs(gv.snowfall_i) * gv.snowl));
+                velc = 0.0f;
+            // watermass_calc_internal(gv,gp,&deltt,&velc,logPULSEfile);
+            } else {// melt       
+                    // Estimate interstitial flow velocity 
+                velc = (gv.qmelt_i + gv.rainfall_i)/ (gv.vfrac_a + gv.vfrac_m); // interstitial flow velocity [m s-1]
+                D = gp.aD * velc;       // dispersion coefficient [m2/s]
+
+                (*gv.velc_2d) = (*gv.velc_2d)*0 + velc;
+                (*gv.disp_2d) = (*gv.disp_2d)*0 + D;
                 
-        findInterpMeteo(gv,&tcum);
-        findInterpQmelt(gv,&tcum);
+                deltt = std::fmin(print_next-tcum,gp.Courant * gv.snowh / velc);
 
-        gv.vfrac_m_prev = gv.vfrac_m;
-        gv.vfrac_s_prev = gv.vfrac_s;
-        //gv.wetfront_cell_prev = gv.wetfront_cell;
-
-        if (gv.qmelt_i==0.0f && gv.rainfall_i == 0.0f){ // accumulation only 
-            deltt = std::fmin(print_next-tcum,
-                gv.v_swe_freshsnow_max/(std::abs(gv.snowfall_i) * gv.snowl));
-            velc = 0.0f;
-           // watermass_calc_internal(gv,gp,&deltt,&velc,logPULSEfile);
-        } else {// melt       
-                // Estimate interstitial flow velocity 
-            velc = (gv.qmelt_i + gv.rainfall_i)/ (gv.vfrac_a + gv.vfrac_m); // interstitial flow velocity [m s-1]
-            D = gp.aD * velc;       // dispersion coefficient [m2/s]
-
-            (*gv.velc_2d) = (*gv.velc_2d)*0 + velc;
-            (*gv.disp_2d) = (*gv.disp_2d)*0 + D;
-            
-            deltt = std::fmin(print_next-tcum,gp.Courant * gv.snowh / velc);
-
-            // limit step so that if there is melt or accumulation it doesn't go more than one cell
-            //deltt_volavail_melt = velc * (*deltt) * max(min((*gv.v_liqwater)));
-            deltt = std::fmin(deltt,gv.v_swe_freshsnow_max/(std::abs(gv.snowfall_i) * gv.snowl));
-            deltt = std::fmin(deltt,gv.v_swe_freshsnow_max/(std::abs(gv.qmelt_i) * gv.snowl));
-            
-            // wetting front
-            //wetfront_calc(gp,gv,&velc,&deltt);   
-            
-        }
-
-        // Snow model: internal or external (SNOWPACK or WARD-snow model)
-        if (gp.snowmodel == 0){ // internal 
-            watermass_calc_internal(gv,gp,&deltt,&velc,logPULSEfile);
-        }else if (gp.snowmodel == 1){ // external
-            watermass_calc_external(gv,gp,&deltt,logPULSEfile);
-        } 
-
-        tcum = tcum + deltt; 
-
-        if ((gv.qmelt_i+gv.rainfall_i)>0.0f && gv.nh>0){ // if melt
- 
-            // calculate volume fractions
-            //vol_fract_calc(gp,gv,&velc,&deltt);
-
-            // check CFC validation
-            //if((gv.wetfront_cell - gv.wetfront_cell_prev) > 1){
-            //    msg = "CFC condition violation - check code";
-            //    print_screen_log(logPULSEfile,&msg);
-            //    abort();
-            //}
-
-            // 
-            //if (gv.vfrac_m < 1-gp.num_stblty_thrshld_prsity && gv.vfrac_i > gp.num_stblty_thrshld_prsity && gv.vfrac_s > gp.num_stblty_thrshld_prsity){
-            if (gv.vfrac_m < 1-gp.num_stblty_thrshld_prsity && gv.vfrac_s > gp.num_stblty_thrshld_prsity){  
+                // limit step so that if there is melt or accumulation it doesn't go more than one cell
+                //deltt_volavail_melt = velc * (*deltt) * max(min((*gv.v_liqwater)));
+                deltt = std::fmin(deltt,gv.v_swe_freshsnow_max/(std::abs(gv.snowfall_i) * gv.snowl));
+                deltt = std::fmin(deltt,gv.v_swe_freshsnow_max/(std::abs(gv.qmelt_i) * gv.snowl));
                 
-                if (gv.wetfront_cell > 3){ // to have sufficient layers for ADE solver
-
-                    if (gp.hydro_solver == 1){
-                        crank_nicholson(gv,&deltt,&velc,&D); // solve advection and dispersion in the mobile zone
-                    }else if (gp.hydro_solver == 0){
-                    //crank_nicholson_hydr2D(gv,&deltt);
-                        FtCs_solve_hydr2D(gv,&deltt);
-
-                    // Crank Nicolson to limit the fluxes across boundaries
-                    //exchange_i = arma::max(v * deltt * ((*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new-1) - (*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new))/gv.snowh,(*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new-1));
-                    //(*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new-1) -= exchange_i; // compute onh advection to the wetting front
-                    //(*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new) += exchange_i; // compute onh advection to the wetting front
-                    }
-                    for(il=0;il<gv.nl ;il++){
-                        for(ih=0;ih<gv.nh ;ih++){
-                            (*gv.c_m).at(il,ih) = fmax((*gv.c_m).at(il,ih),0.0f);
-                        }
-                    }
-
-              }
+                // wetting front
+                //wetfront_calc(gp,gv,&velc,&deltt);   
+                
             }
 
-            if (gv.qmelt_i > 0.0f || gv.rainfall_i > 0.0f){
+            
+            
+            watermass_calc_internal(gv,gp,&deltt,&velc,logPULSEfile);
+        
 
-                 // Ion exclusion: Exchange with immobile phase (just exchange)
-                (*gv.exchange_is)  = deltt * gp.alphaIE * (*gv.c_s) % (*gv.v_swe) ; 
-                float exchange_is_l = 0.0f;
+            tcum = tcum + deltt; 
 
-                // limit the flux to the available material
-                for(il=0;il<gv.nl ;il++){
-                    for(ih=0;ih<gv.wetfront_cell ;ih++){
-                         if ((*gv.exchange_is).at(il,ih) > 0 && (*gv.v_liqwater).at(il,ih) > 0.0001){
+            if ((gv.qmelt_i+gv.rainfall_i)>0.0f && gv.nh>0){ // if melt
+    
+                // calculate volume fractions
+                //vol_fract_calc(gp,gv,&velc,&deltt);
 
-                             //std::cout << std::to_string((*gv.v_liqwater).at(il,ih)) << std::endl;
-                            exchange_is_l = fmin((*gv.exchange_is).at(il,ih),
-                                                 (*gv.c_s).at(il,ih) * (*gv.v_swe).at(il,ih));   
+                // check CFC validation
+                //if((gv.wetfront_cell - gv.wetfront_cell_prev) > 1){
+                //    msg = "CFC condition violation - check code";
+                //    print_screen_log(logPULSEfile,&msg);
+                //    abort();
+                //}
 
-                            (*gv.c_s).at(il,ih) = ((*gv.c_s).at(il,ih) * (*gv.v_swe).at(il,ih) 
-                                    - exchange_is_l) / (*gv.v_swe).at(il,ih);
+                // 
+                //if (gv.vfrac_m < 1-gp.num_stblty_thrshld_prsity && gv.vfrac_i > gp.num_stblty_thrshld_prsity && gv.vfrac_s > gp.num_stblty_thrshld_prsity){
+                if (gv.vfrac_m < 1-gp.num_stblty_thrshld_prsity && gv.vfrac_s > gp.num_stblty_thrshld_prsity){  
+                    
+                    if (gv.wetfront_cell > 3){ // to have sufficient layers for ADE solver
 
-                            (*gv.c_m).at(il,ih) = ((*gv.c_m).at(il,ih) * (*gv.v_liqwater).at(il,ih) 
-                                    + exchange_is_l) / (*gv.v_liqwater).at(il,ih);
+                        if (gp.hydro_solver == 1){
+                            crank_nicholson(gv,&deltt,&velc,&D); // solve advection and dispersion in the mobile zone
+                        }else if (gp.hydro_solver == 0){
+                        //crank_nicholson_hydr2D(gv,&deltt);
+                            FtCs_solve_hydr2D(gv,&deltt);
 
-                            (*gv.c_s).at(il,ih) = std::fmax((*gv.c_s).at(il,ih),0.0f);
-                            (*gv.c_m).at(il,ih) = std::fmax((*gv.c_m).at(il,ih),0.0f);
-                            
-
-                        //}else if((*gv.exchange_is).at(il,ih) < 0){
-                        //     (*gv.exchange_is).at(il,ih) = 0.0f;
-                             //msg = "PROBLEM: negative s->i exchange" + std::to_string((*gv.c_s).at(il,ih));
-                             //std::cout << msg << std::endl;
-                             //print_screen_log(logPULSEfile,&msg);
+                        // Crank Nicolson to limit the fluxes across boundaries
+                        //exchange_i = arma::max(v * deltt * ((*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new-1) - (*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new))/gv.snowh,(*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new-1));
+                        //(*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new-1) -= exchange_i; // compute onh advection to the wetting front
+                        //(*gv.c_m)(arma::span(0,gv.nl-1),wetfront_cell_new) += exchange_i; // compute onh advection to the wetting front
                         }
-                         //if(ih<gv.upperboundary_cell || ih>gv.wetfront_cell){
-                        //if(ih>gv.wetfront_cell){
-                        //    (*gv.exchange_is).at(il,ih) = 0.0f;
-                        //};
-                    }
-                };
-             
-                //(*gv.c_s) =  ( (*gv.c_s) * gv.vfrac_s - (*gv.exchange_is)) / gv.vfrac_s; // / vfrac_m(t);
-                //(*gv.c_m) = ( (*gv.c_m) * gv.vfrac_m + (*gv.exchange_is)) / gv.vfrac_m;
-  
+                        for(il=0;il<gv.nl ;il++){
+                            for(ih=0;ih<gv.nh ;ih++){
+                                (*gv.c_m).at(il,ih) = fmax((*gv.c_m).at(il,ih),0.0f);
+                            }
+                        }
 
-                
-                // Snowmelt
-                //(*gv.exchange_si) = (*gv.c_s) * gv.qmelt_i * deltt / (gv.wetfront_cell+1);
+                }
+                }
 
-                // limit the flux to the available material
-                //for(il=0;il<gv.nl ;il++){
-                //    for(ih=0;ih<gv.nh ;ih++){
+                if (gv.qmelt_i > 0.0f || gv.rainfall_i > 0.0f){
 
-                //            if ((*gv.exchange_si).at(il,ih) > 0 && ih<gv.wetfront_cell){
-                //                (*gv.exchange_si).at(il,ih) = std::fmin((*gv.exchange_si).at(il,ih),(*gv.c_s).at(il,ih));
-                            
-                //                (*gv.c_s).at(il,ih) = ((*gv.c_s).at(il,ih) * gv.vfrac_s 
-                //                    - (*gv.exchange_si).at(il,ih)) / gv.vfrac_s;
-                //                (*gv.c_m).at(il,ih) = ((*gv.c_m).at(il,ih) * gv.vfrac_m 
-                //                        + (*gv.exchange_si).at(il,ih)) / gv.vfrac_m;
-                //                (*gv.c_s).at(il,ih) = std::fmax((*gv.c_s).at(il,ih),0.0f);
-                //                (*gv.c_m).at(il,ih) = std::fmax((*gv.c_m).at(il,ih),0.0f);
-                            
-                //            }else if((*gv.exchange_si).at(il,ih) < 0){
-                                //msg = "PROBLEM: negative s->i exchange";
+                    // Ion exclusion: Exchange with immobile phase (just exchange)
+                    (*gv.exchange_is)  = deltt * gp.alphaIE * (*gv.c_s) % (*gv.v_swe) ; 
+                    float exchange_is_l = 0.0f;
+
+                    // limit the flux to the available material
+                    for(il=0;il<gv.nl ;il++){
+                        for(ih=0;ih<gv.wetfront_cell ;ih++){
+                            if ((*gv.exchange_is).at(il,ih) > 0 && (*gv.v_liqwater).at(il,ih) > 0.0001){
+
+                                //std::cout << std::to_string((*gv.v_liqwater).at(il,ih)) << std::endl;
+                                exchange_is_l = fmin((*gv.exchange_is).at(il,ih),
+                                                    (*gv.c_s).at(il,ih) * (*gv.v_swe).at(il,ih));   
+
+                                (*gv.c_s).at(il,ih) = ((*gv.c_s).at(il,ih) * (*gv.v_swe).at(il,ih) 
+                                        - exchange_is_l) / (*gv.v_swe).at(il,ih);
+
+                                (*gv.c_m).at(il,ih) = ((*gv.c_m).at(il,ih) * (*gv.v_liqwater).at(il,ih) 
+                                        + exchange_is_l) / (*gv.v_liqwater).at(il,ih);
+
+                                (*gv.c_s).at(il,ih) = std::fmax((*gv.c_s).at(il,ih),0.0f);
+                                (*gv.c_m).at(il,ih) = std::fmax((*gv.c_m).at(il,ih),0.0f);
+                                
+
+                            //}else if((*gv.exchange_is).at(il,ih) < 0){
+                            //     (*gv.exchange_is).at(il,ih) = 0.0f;
+                                //msg = "PROBLEM: negative s->i exchange" + std::to_string((*gv.c_s).at(il,ih));
                                 //std::cout << msg << std::endl;
                                 //print_screen_log(logPULSEfile,&msg);
-                //            }
+                            }
+                            //if(ih<gv.upperboundary_cell || ih>gv.wetfront_cell){
                             //if(ih>gv.wetfront_cell){
-                            //    (*gv.exchange_si).at(il,ih) = 0.0f;
+                            //    (*gv.exchange_is).at(il,ih) = 0.0f;
                             //};
-                //    }
-                //};
-                //(*gv.c_m) =  ( (*gv.c_m) * gv.vfrac_m_prev + (*gv.exchange_si) * gv.vfrac_s_prev ) / gv.vfrac_m; // / vfrac_m(t);
-                //(*gv.c_s) = ( (*gv.c_s) * gv.vfrac_s_prev - (*gv.exchange_si) * gv.vfrac_s_prev ) / gv.vfrac_s;
+                        }
+                    };
                 
+                    //(*gv.c_s) =  ( (*gv.c_s) * gv.vfrac_s - (*gv.exchange_is)) / gv.vfrac_s; // / vfrac_m(t);
+                    //(*gv.c_m) = ( (*gv.c_m) * gv.vfrac_m + (*gv.exchange_is)) / gv.vfrac_m;
+    
+
+                    
+                    // Snowmelt
+                    //(*gv.exchange_si) = (*gv.c_s) * gv.qmelt_i * deltt / (gv.wetfront_cell+1);
+
+                    // limit the flux to the available material
+                    //for(il=0;il<gv.nl ;il++){
+                    //    for(ih=0;ih<gv.nh ;ih++){
+
+                    //            if ((*gv.exchange_si).at(il,ih) > 0 && ih<gv.wetfront_cell){
+                    //                (*gv.exchange_si).at(il,ih) = std::fmin((*gv.exchange_si).at(il,ih),(*gv.c_s).at(il,ih));
+                                
+                    //                (*gv.c_s).at(il,ih) = ((*gv.c_s).at(il,ih) * gv.vfrac_s 
+                    //                    - (*gv.exchange_si).at(il,ih)) / gv.vfrac_s;
+                    //                (*gv.c_m).at(il,ih) = ((*gv.c_m).at(il,ih) * gv.vfrac_m 
+                    //                        + (*gv.exchange_si).at(il,ih)) / gv.vfrac_m;
+                    //                (*gv.c_s).at(il,ih) = std::fmax((*gv.c_s).at(il,ih),0.0f);
+                    //                (*gv.c_m).at(il,ih) = std::fmax((*gv.c_m).at(il,ih),0.0f);
+                                
+                    //            }else if((*gv.exchange_si).at(il,ih) < 0){
+                                    //msg = "PROBLEM: negative s->i exchange";
+                                    //std::cout << msg << std::endl;
+                                    //print_screen_log(logPULSEfile,&msg);
+                    //            }
+                                //if(ih>gv.wetfront_cell){
+                                //    (*gv.exchange_si).at(il,ih) = 0.0f;
+                                //};
+                    //    }
+                    //};
+                    //(*gv.c_m) =  ( (*gv.c_m) * gv.vfrac_m_prev + (*gv.exchange_si) * gv.vfrac_s_prev ) / gv.vfrac_m; // / vfrac_m(t);
+                    //(*gv.c_s) = ( (*gv.c_s) * gv.vfrac_s_prev - (*gv.exchange_si) * gv.vfrac_s_prev ) / gv.vfrac_s;
+                    
+            }
         }
-    }
+    } else if (gp.snowmodel == 1){ // external
+        
+        //!!!!!!!!!!!!! CONTINUE HERE
+
+        watermass_calc_external(gv,gp,&deltt,logPULSEfile);
+
+    } 
 
     // Print results                
         if (tcum>=print_next){
