50df71bfd78c86747bdc4d1f0dcc6b0ef3b9f08c
DiogoCostaPT <1208buckas>
2020-10-22 15:12:35 -0600

    cleaning comments and fix closing } from two commitments before

--- src/pulse.cpp
+++ src/pulse.cpp
@@ -8,35 +8,34 @@
 void pulsemodel(globalpar& gp,globalvar& gv,std::ofstream* logPULSEfile,
         std::string* results_flname)
 {
-// initiation
-double tcum = 0.f,
-        //q = 0.f, // melt volume/int
-        t = 1.f, 
-        deltt = 1.0f, // time step calculated from the CFL condition
-        velc = 0.0f, // interstitial flow velocity [m s-1]
-        D = 0.0f; // dispersion coefficient [m2/s]
-std::string msg;  
-std::chrono::duration<double> elapsed_seconds;
-auto start = std::chrono::system_clock::now();
-auto end = std::chrono::system_clock::now();
-bool outwritestatus;
-arma::mat exchange_i;
+    // initiation
+    double tcum = 0.f,
+            //q = 0.f, // melt volume/int
+            t = 1.f, 
+            deltt = 1.0f, // time step calculated from the CFL condition
+            velc = 0.0f, // interstitial flow velocity [m s-1]
+            D = 0.0f; // dispersion coefficient [m2/s]
+    std::string msg;  
+    std::chrono::duration<double> elapsed_seconds;
+    auto start = std::chrono::system_clock::now();
+    auto end = std::chrono::system_clock::now();
+    bool outwritestatus;
+    arma::mat exchange_i;
 
-unsigned int il,ih,print_next;
-    
-tcum = gv.timstart;
-print_next = tcum + gp.print_step;
+    unsigned int il,ih,print_next;
+        
+    tcum = gv.timstart;
+    print_next = tcum + gp.print_step;
 
 
-while (tcum < gp.Tsim)
+    while (tcum < gp.Tsim)
     {
-    
-    t += 1;
+        
+        t += 1;
 
-    // Snow model: internal or external (SNOWPACK or WARD-snow model)
-    if (gp.snowmodel == 0){ // internal 
-            
-                    
+        // Snow model: internal or external (SNOWPACK or WARD-snow model)
+        if (gp.snowmodel == 0){ // internal    
+                        
             findInterpMeteo(gv,&tcum);
             findInterpQmelt(gv,&tcum);
 
@@ -69,7 +68,6 @@ while (tcum < gp.Tsim)
                 
             }
 
-            
             
             watermass_calc_internal(gv,gp,&deltt,&velc,logPULSEfile);
         
@@ -111,7 +109,7 @@ while (tcum < gp.Tsim)
                             }
                         }
 
-                }
+                    }
                 }
 
                 if (gv.qmelt_i > 0.0f || gv.rainfall_i > 0.0f){
@@ -136,90 +134,43 @@ while (tcum < gp.Tsim)
                                         + exchange_is_l) / (*gv.v_liqwater).at(il,ih);
 
                                 (*gv.c_s).at(il,ih) = std::fmax((*gv.c_s).at(il,ih),0.0f);
-                                (*gv.c_m).at(il,ih) = std::fmax((*gv.c_m).at(il,ih),0.0f);
-                                
+                                (*gv.c_m).at(il,ih) = std::fmax((*gv.c_m).at(il,ih),0.0f);  
 
-                            //}else if((*gv.exchange_is).at(il,ih) < 0){
-                            //     (*gv.exchange_is).at(il,ih) = 0.0f;
-                                //msg = "PROBLEM: negative s->i exchange" + std::to_string((*gv.c_s).at(il,ih));
-                                //std::cout << msg << std::endl;
-                                //print_screen_log(logPULSEfile,&msg);
                             }
-                            //if(ih<gv.upperboundary_cell || ih>gv.wetfront_cell){
-                            //if(ih>gv.wetfront_cell){
-                            //    (*gv.exchange_is).at(il,ih) = 0.0f;
-                            //};
-                        }
-                    };
-                
-                    //(*gv.c_s) =  ( (*gv.c_s) * gv.vfrac_s - (*gv.exchange_is)) / gv.vfrac_s; // / vfrac_m(t);
-                    //(*gv.c_m) = ( (*gv.c_m) * gv.vfrac_m + (*gv.exchange_is)) / gv.vfrac_m;
-    
-
+                        };
                     
-                    // Snowmelt
-                    //(*gv.exchange_si) = (*gv.c_s) * gv.qmelt_i * deltt / (gv.wetfront_cell+1);
-
-                    // limit the flux to the available material
-                    //for(il=0;il<gv.nl ;il++){
-                    //    for(ih=0;ih<gv.nh ;ih++){
-
-                    //            if ((*gv.exchange_si).at(il,ih) > 0 && ih<gv.wetfront_cell){
-                    //                (*gv.exchange_si).at(il,ih) = std::fmin((*gv.exchange_si).at(il,ih),(*gv.c_s).at(il,ih));
-                                
-                    //                (*gv.c_s).at(il,ih) = ((*gv.c_s).at(il,ih) * gv.vfrac_s 
-                    //                    - (*gv.exchange_si).at(il,ih)) / gv.vfrac_s;
-                    //                (*gv.c_m).at(il,ih) = ((*gv.c_m).at(il,ih) * gv.vfrac_m 
-                    //                        + (*gv.exchange_si).at(il,ih)) / gv.vfrac_m;
-                    //                (*gv.c_s).at(il,ih) = std::fmax((*gv.c_s).at(il,ih),0.0f);
-                    //                (*gv.c_m).at(il,ih) = std::fmax((*gv.c_m).at(il,ih),0.0f);
-                                
-                    //            }else if((*gv.exchange_si).at(il,ih) < 0){
-                                    //msg = "PROBLEM: negative s->i exchange";
-                                    //std::cout << msg << std::endl;
-                                    //print_screen_log(logPULSEfile,&msg);
-                    //            }
-                                //if(ih>gv.wetfront_cell){
-                                //    (*gv.exchange_si).at(il,ih) = 0.0f;
-                                //};
-                    //    }
-                    //};
-                    //(*gv.c_m) =  ( (*gv.c_m) * gv.vfrac_m_prev + (*gv.exchange_si) * gv.vfrac_s_prev ) / gv.vfrac_m; // / vfrac_m(t);
-                    //(*gv.c_s) = ( (*gv.c_s) * gv.vfrac_s_prev - (*gv.exchange_si) * gv.vfrac_s_prev ) / gv.vfrac_s;
-                    
+                    }
+                } 
             }
-        }
-    } else if (gp.snowmodel == 1){ // external
-        
-        //!!!!!!!!!!!!! CONTINUE HERE
+        } else if (gp.snowmodel == 1){ // external
+            
 
-        watermass_calc_external(gv,gp,&deltt,logPULSEfile);
+            watermass_calc_external(gv,gp,&deltt,logPULSEfile);
 
-    } 
+        } 
 
-    // Print results                
-        if (tcum>=print_next){
+        // Print results                
+            if (tcum>=print_next){
 
-            end = std::chrono::system_clock::now();
-            elapsed_seconds = end-start;
+                end = std::chrono::system_clock::now();
+                elapsed_seconds = end-start;
 
-            outwritestatus = print_results(gv,gp,std::round(print_next),gp.print_step,elapsed_seconds,
-                    results_flname);
+                outwritestatus = print_results(gv,gp,std::round(print_next),gp.print_step,elapsed_seconds,
+                        results_flname);
 
-        if(outwritestatus == true) 
-        {
-            std::cout << "Saved: '" << print_next << ".txt' || time step (min): " << std::to_string(gp.print_step/60) << " || Time elapsed (min): " << elapsed_seconds.count()/60 << std::endl;
-            (*logPULSEfile) << "Saved: '" << print_next << ".txt' || time step (min): " << std::to_string(gp.print_step/60) << " || Time elapsed (min): " << std::to_string(elapsed_seconds.count()/60) + "\n";
-            print_next += gp.print_step;
-            start = std::chrono::system_clock::now();
-        } else
-        {
-            msg = "Problem when saving the results:" + print_next;
-            print_screen_log(logPULSEfile,&msg);
-            abort();
-        }
-        }
+            if(outwritestatus == true) 
+            {
+                std::cout << "Saved: '" << print_next << ".txt' || time step (min): " << std::to_string(gp.print_step/60) << " || Time elapsed (min): " << elapsed_seconds.count()/60 << std::endl;
+                (*logPULSEfile) << "Saved: '" << print_next << ".txt' || time step (min): " << std::to_string(gp.print_step/60) << " || Time elapsed (min): " << std::to_string(elapsed_seconds.count()/60) + "\n";
+                print_next += gp.print_step;
+                start = std::chrono::system_clock::now();
+            } else
+            {
+                msg = "Problem when saving the results:" + print_next;
+                print_screen_log(logPULSEfile,&msg);
+                abort();
+            }
+            }
 
-    }
-       
+    }   
 }
